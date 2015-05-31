2e159349aaaec88433145c1ece10bc79fdc14fb0
z80020100 <z800201002005@gmail.com>
2015-05-31 20:22:47 +0800

    Remove duplicated ID and timeing

--- File_ID_MD5/main.cpp
+++ File_ID_MD5/main.cpp
@@ -9,6 +9,10 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <windows.h>
 #include <dirent.h>
 
 #include <md5.h>
@@ -25,8 +29,8 @@ int main()
 	int p1, p2, p3, p4;
 	string file_name;
 
-	fstream comp_file;
-	string comp_path = "./Comparison_Table.txt";
+	fstream comp_file, dup_log;
+	string comp_path = "./Comparison_Table.txt", dup_path = "./Duplicated_List.txt";
 
 	DIR *dp;
 	struct dirent *ep;
@@ -38,6 +42,15 @@ int main()
 	HexEncoder encoder;
 	string output;
 
+	vector<int> v;
+	vector<int>::iterator iter;
+
+	LARGE_INTEGER startTime, endTime, fre;
+	double times;
+
+	QueryPerformanceFrequency(&fre); // 取得CPU頻率
+	QueryPerformanceCounter(&startTime); // 取得開機到現在經過幾個CPU Cycle
+	/* Program to Timing */
 	dp = opendir(doc_path.c_str()); // for each file f, create a list f_bar of unique keyword
 	if (dp != NULL)
 	{
@@ -48,13 +61,17 @@ int main()
 			cerr << "Error: open" << comp_path << " failed..." << endl;
 		else
 		{
+			dup_log.open(dup_path, ios::out);
+			if (!dup_log)
+				cerr << "Error: can not create " << dup_path << endl;
+
 			comp_file << "File name                      ";
 			comp_file << left << ", " << "ID        " << ", MD5 " << endl;;
 			
 			while (ep = readdir(dp))
 			{
 				output.clear();
-				printf("Processing file: %s\n", ep->d_name);
+				//printf("Processing file: %s\n", ep->d_name);
 				file_name.assign(ep->d_name);
 				hash.CalculateDigest(digest, (byte*)file_name.c_str(), file_name.length());
 
@@ -62,7 +79,7 @@ int main()
 				encoder.Put(digest, sizeof(digest));
 				encoder.MessageEnd();
 
-				cout << output << std::endl;
+				//cout << output << std::endl;
 
 				strncpy((char*)&p1, (char*)&digest[0], 4);
 				strncpy((char*)&p2, (char*)&digest[4], 4);
@@ -70,16 +87,36 @@ int main()
 				strncpy((char*)&p4, (char*)&digest[12], 4);
 
 				ID = p1 ^ p2 ^ p3 ^ p4;
-				cout << ID << endl;
+				iter = find(v.begin(), v.end(), ID);
+				if (iter == v.end())
+				{
+					v.push_back(ID);
+					//cout << ID << endl;
 
-				comp_file.width(32);
-				comp_file << left << file_name << ", ";
-				comp_file.width(11);
-				comp_file << right << ID << ", " << output << endl;
+					comp_file.width(32);
+					comp_file << left << file_name << ", ";
+					comp_file.width(11);
+					comp_file << right << ID << ", " << output << endl;
+				}
+				else
+				{
+					cout << "Duplicated ID: " << ID << endl;
+					cout << "    File name: " << file_name << endl << endl;
+					dup_log << "Duplicated ID: " << ID << endl;
+					dup_log << "    File name: " << file_name << endl << endl;
+				}
+
 			}
 			comp_file.close();
 		}
 	}
+
+	QueryPerformanceCounter(&endTime); // 取得開機到程式執行完成經過幾個CPU Cycle
+	times = ((double)endTime.QuadPart - (double)startTime.QuadPart) / fre.QuadPart;
+
+	cout << fixed << setprecision(3) << "Processing time: " << times << 's' << endl << endl;
+	dup_log << fixed << setprecision(3) << "Processing time: " << times << 's' << endl << endl;
+	dup_log.close();
 
 	system("PAUSE");
 	return 0;
